9c2dd312c774fb3fff9ad5e74c9fab546c0f9a63
jiawei-mo <kimiwings@gmail.com>
2016-06-30 13:43:19 -0500

    remove Debug dir check

--- Debug/src/gsvFitcher.d
+++
@@ -1,6 +0,0 @@
-src/gsvFitcher.d: ../src/gsvFitcher.cpp ../src/gsvFetcher.hpp \
- ../src/parameters.hpp
-
-../src/gsvFetcher.hpp:
-
-../src/parameters.hpp:
--- Debug/src/tracker.d
+++
@@ -1,6 +0,0 @@
-src/tracker.d: ../src/tracker.cpp ../src/tracker.hpp \
- ../src/parameters.hpp
-
-../src/tracker.hpp:
-
-../src/parameters.hpp:
